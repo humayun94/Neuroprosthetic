0a84d5c335a3d293d54b7e47f05b0cb818a737fd
humayun94 <31209060+humayun94@users.noreply.github.com>
2017-08-21 16:22:34 +0500

    Add files via upload

---
+++ Prosthetic_code.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+Servo thumb, fingers;
+
+int analogpin = 3;
+int val = 0;
+
+void setup() 
+{
+  fingers.attach(5);
+  thumb.attach(8);
+  Serial.begin(9600);
+
+}
+
+void loop() 
+{
+  val = analogRead(analogpin);
+  Serial.println(val);
+  if ( val>800)
+  {
+    thumb.write(180);
+    fingers.write(180);
+    delay(100);
+  }
+ else{
+    thumb.write(0);
+    fingers.write(0);
+    delay(100);
+    }
+}
